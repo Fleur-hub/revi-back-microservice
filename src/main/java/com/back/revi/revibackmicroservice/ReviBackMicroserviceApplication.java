247aad18cd9cabd3c03ed43bf4373d6f64303419
Elodie ROUSSEL <roussel.elodie98@gmail.com>
2022-04-29 20:28:45 +0200

    feat: init project

---
+++ src/main/java/com/back/revi/revibackmicroservice/ReviBackMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.back.revi.revibackmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviBackMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviBackMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/revi/revibackmicroservice/ReviBackMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.revi.revibackmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviBackMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
